99fe0e84c3d65cb5093fbd6977a37237469fc245
anusha bandaru <bandaru.a@husky.neu.edu>
2015-10-02 15:58:10 -0400

    Committing FizzBuzz

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,9 +6,30 @@
 		{
 			string result = number.ToString();
 
-			// your solution/implementation should go in here
+            // your solution/implementation should go in here
+            if (number == 0)
+            {
+                return "0";
+            }
 
-			return result;
+            if (number % 3 == 0)
+            {
+                System.Console.WriteLine("Fizz");
+                result = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                System.Console.WriteLine("Buzz");
+                result = "Buzz";
+            }
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                System.Console.WriteLine("FizzBuzz");
+                result = "FizzBuzz";
+            }
+
+            return result;
+            
 		}
 	}
 }
