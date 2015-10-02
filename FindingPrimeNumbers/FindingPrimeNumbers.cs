13babd2be9efd67b9ddca88de3dda2ea4e3dc5e5
anusha bandaru <bandaru.a@husky.neu.edu>
2015-10-02 15:49:28 -0400

    Comitting Prime numbers

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -4,11 +4,30 @@
 	{
 		public long FindSumOfPrimeNumbers(int n)
 		{
-			long sum = 0;
+            // your solution goes in here
+            long sum = 0;
+            int j, count = 0, number = 2, flag = 1;
 
-			// your solution goes in here
-
-			return sum;
+            while (count < n)
+            {
+                for (j = 2; j <= (number / 2); j++)
+                {
+                    if (number % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                    flag = 1;
+                }
+                if (flag != 0)
+                {
+                    sum = sum + number;
+                    count++;
+                }
+                number++;
+            }
+            System.Console.WriteLine("Sum Is" + sum);
+            return sum;
 		}
 	}
 }
