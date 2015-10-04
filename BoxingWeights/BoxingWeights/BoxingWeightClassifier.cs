0a211f16e1df18a3bf3532173f773eb2c406b594
anusha bandaru <bandaru.a@husky.neu.edu>
2015-10-04 00:27:51 -0400

    Committing BoxingWeights

--- BoxingWeights/BoxingWeights/BoxingWeightClassifier.cs
+++ BoxingWeights/BoxingWeights/BoxingWeightClassifier.cs
@@ -1,14 +1,101 @@
 ﻿namespace BoxingWeights
 {
-	public class BoxingWeightClassifier
-	{
-		public string ClassifyBoxingWeight(int weightInPounds)
-		{
-			string result = weightInPounds.ToString();
+    public class BoxingWeightClassifier
+    {
+        public string ClassifyBoxingWeight(int weightInPounds)
+        {
+            string result = weightInPounds.ToString();
+            // your solution/implementation should go in here
 
-			// your solution/implementation should go in here
+            if ((weightInPounds >= 0) && (weightInPounds <= 105))
+            {
+                result = Constants.Strawweight + " and " + Constants.Heavyweight;
 
-			return result;
-		}
-	}
-}
+            }
+            else if ((weightInPounds > 105) && (weightInPounds <= 108))
+            {
+                result = Constants.JuniorFlyweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 108) && (weightInPounds <= 112))
+            {
+                result = Constants.Flyweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 112) && (weightInPounds <= 115))
+            {
+                result = Constants.JuniorBantamweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 115) && (weightInPounds <= 118))
+            {
+                result = Constants.Bantamweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 118) && (weightInPounds <= 122))
+            {
+                result = Constants.JuniorFeatherweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 122) && (weightInPounds <= 126))
+            {
+                result = Constants.Featherweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 126) && (weightInPounds <= 130))
+            {
+                result = Constants.JuniorLightweight + " and " + Constants.Heavyweight;
+            }
+
+            else if ((weightInPounds > 130) && (weightInPounds <= 135))
+            {
+                result = Constants.Lightweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 135) && (weightInPounds <= 140))
+            {
+                result = Constants.JuniorWelterweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 140) && (weightInPounds <= 147))
+            {
+                result = Constants.Welterweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 147) && (weightInPounds <= 154))
+            {
+                result = Constants.JuniorMiddleweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 154) && (weightInPounds <= 160))
+            {
+                result = Constants.Middleweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 160) && (weightInPounds <= 168))
+            {
+                result = Constants.SuperMiddleweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 168) && (weightInPounds <= 175))
+            {
+                result = Constants.LightHeavyweight + " and " + Constants.Heavyweight;
+
+            }
+            else if ((weightInPounds > 175) && (weightInPounds <= 200))
+            {
+                result = Constants.Cruiserweight + " and " + Constants.Heavyweight;
+
+            }
+            else
+                result = Constants.Heavyweight;
+
+                return result;
+            }
+        }
+    }
+
+            
+
+          
+
--- BoxingWeights/BoxingWeights/Program.cs
+++ BoxingWeights/BoxingWeights/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace BoxingWeights
+﻿using System;
+using BoxingWeights;
+
+
+class Program
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
-}
+    static void Main(string[] args)
+    {
+    Program p = new Program();
+        System.Console.WriteLine("Enter weight");
+        int weight = Convert.ToInt32(Console.ReadLine());
+        string result;
+        BoxingWeightClassifier boxWghtClass = new BoxingWeightClassifier();
+        result=boxWghtClass.ClassifyBoxingWeight(weight);
+        Console.WriteLine(result.ToString());
+        Console.ReadLine();
+        
+    }  
+    
+        }
+    
+
+    
+ 
