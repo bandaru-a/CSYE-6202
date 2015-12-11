e798d8874c6045ad8f596f9244ae6e8fd189078a
anusha bandaru <bandaru.a@husky.neu.edu>
2015-12-11 15:42:57 -0500

    Second Commit

---
+++ AirLine/Airline Reservation/TestProject/WhenRunningFinalProject.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+  [TestFixture]
+    public class WhenRunningFinalProject
+    {
+
+    public void WhenRunningTest()
+    {
+      Assert.True(true);
+    }
+    }
+}
